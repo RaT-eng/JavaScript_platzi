e5e33786933a415665887d0964e25e96795a9878
RaT-eng <razieltorres7@gmail.com>
2023-04-09 17:20:22 -0600

    Primer commit

---
+++ function.js
@@ -0,0 +1,8 @@
+//declarative fuction
+function miFuncion(){
+
+}
+//Expresion || Anonymous function
+const miFuncion = function(a,b){
+    return a + b
+}
---
+++ valores.js
@@ -0,0 +1,14 @@
+/*Valores primitivos*/
+40 //Number
+"hola"//String
+true //boolean
+false //boolean
+Null //
+undefined//
+
+/*Type object*/
+[1,2,3] // Array
+{name: "Raziel"} // Object
+
+//who to see types
+typeof("type")
